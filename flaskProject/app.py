0d4d91564a6ef19796c7242f2a60ab4bd38ff8e8
Nilbog24 <19342@wc.school.nz>
2023-03-21 11:55:05 +1300

    First Commit Added CSS and Base

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -1,11 +1,19 @@
-from flask import Flask
+import sqlite3
+from sqlite3 import Error
+from flask import Flask, render_template, request, redirect
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def create_connection(db_file):
+    try:
+        connection = sqlite3.connect(db_file)
+        return connection
+    except Error as e:
+        print(e)
+    return None
 
 
 if __name__ == '__main__':
